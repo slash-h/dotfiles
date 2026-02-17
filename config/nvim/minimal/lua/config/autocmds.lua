e26472a55951052bd9c280d9e8b9100c7feedf28
slash-h <sudarshan.cbs@gmail.com>
2026-02-17 19:27:43 +0100

    MB: nvim autoformat on save

--- config/nvim/minimal/lua/config/autocmds.lua
+++ config/nvim/minimal/lua/config/autocmds.lua
@@ -1,25 +1,25 @@
 -- autocmd
 --------------------------------------------------------------------------------
 -- Highlight when yanking
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
 -- CDS LANGUAGE SUPPORT
 vim.api.nvim_create_autocmd("User", {
-  pattern = "TSUpdate",
-  callback = function()
-    require("nvim-treesitter.parsers").cds = {
-      install_info = {
-        url = "https://github.com/cap-js-community/tree-sitter-cds.git",
-        -- revision = "HEAD",
-        branch = "main",
-      },
-    }
-  end,
+	pattern = "TSUpdate",
+	callback = function()
+		require("nvim-treesitter.parsers").cds = {
+			install_info = {
+				url = "https://github.com/cap-js-community/tree-sitter-cds.git",
+				-- revision = "HEAD",
+				branch = "main",
+			},
+		}
+	end,
 })
 
 -- FILETYPES
@@ -27,42 +27,55 @@ vim.filetype.add({ extension = { cds = "cds" } })
 
 -- syntax highlighting are not auto-enabled, set it here.
 -- More details: https://github.com/nvim-treesitter/nvim-treesitter#supported-features
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'cds' },
-  callback = function() vim.treesitter.start() end,
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "cds" },
+	callback = function()
+		vim.treesitter.start()
+	end,
 })
 
 -- Enable LSP servers support by filetype
 vim.lsp.enable({
-    "javascript",
-    "cds",
-    "lua_ls",
+	"javascript",
+	"cds",
+	"lua_ls",
 })
 
 -- LSP-based completion support
-vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
-      vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-      vim.keymap.set('i', '<C-Space>', function()
-        vim.lsp.completion.get()
-      end)
-    end
-  end,
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(ev)
+		local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+			vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
+			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+			vim.keymap.set("i", "<C-Space>", function()
+				vim.lsp.completion.get()
+			end)
+		end
+	end,
 })
 
+-- Auto-format on save
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+	callback = function(args)
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			buffer = args.buf,
+			callback = function()
+				vim.lsp.buf.format({ async = false, id = args.data.client_id })
+			end,
+		})
+	end,
+})
 
 -- Diagnostics
 vim.diagnostic.config({
-  -- Use the default configuration
-  -- virtual_lines = true
+	-- Use the default configuration
+	-- virtual_lines = true
 
-  -- Alternatively, customize specific options
-  virtual_lines = {
-    -- Only show virtual line diagnostics for the current cursor line
-    current_line = true,
-  },
+	-- Alternatively, customize specific options
+	virtual_lines = {
+		-- Only show virtual line diagnostics for the current cursor line
+		current_line = true,
+	},
 })
-
